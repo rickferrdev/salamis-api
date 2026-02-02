981cac0e0197fe5f149574b158871c53804e58cd
Henr. Ferr <rickferr.dev@gmail.com>
2026-02-01 22:34:23 -0300

    feat(ports): add interfaces and types for authentication, posts, and profiles

---
+++ internal/core/ports/interfaces.go
@@ -0,0 +1,37 @@
+package ports
+
+import (
+	"context"
+
+	"github.com/rickferrdev/salamis-api/internal/core/domain"
+)
+
+type AuthStorage interface {
+	Create(ctx context.Context, user domain.UserDomain) (*domain.UserDomain, error)
+	FindUserByEmail(ctx context.Context, email string) (*domain.UserDomain, error)
+}
+
+type AuthService interface {
+	Login(ctx context.Context, input AuthInput) (*AuthOutput, error)
+	Register(ctx context.Context, input AuthInput) (*AuthOutput, error)
+}
+
+type PostService interface {
+	Publish(ctx context.Context, post domain.PostDomain) (*PostOutput, error)
+	Delete(ctx context.Context, id uint) error
+}
+
+type PostStorage interface {
+	CreatePost(ctx context.Context, post domain.PostDomain) (*domain.PostDomain, error)
+	FindPostByID(ctx context.Context, id uint) (*domain.PostDomain, error)
+}
+
+type ProfileService interface {
+	UpdateProfile(ctx context.Context, profile domain.ProfileDomain) (*ProfileOutput, error)
+	GetProfileByUserID(ctx context.Context, userID uint) (*ProfileOutput, error)
+}
+
+type ProfileStorage interface {
+	UpdateProfile(ctx context.Context, profile domain.ProfileDomain) (*domain.ProfileDomain, error)
+	FindProfileByUserID(ctx context.Context, userID uint) (*domain.ProfileDomain, error)
+}
