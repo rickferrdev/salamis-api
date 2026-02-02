4ad25ada6817fab7a1ebcc12960cabd7c59ec592
Henr. Ferr <rickferr.dev@gmail.com>
2026-02-02 10:17:32 -0300

    feat: add environment configuration structure and loading function

---
+++ config/env.go
@@ -0,0 +1,24 @@
+package config
+
+import (
+	"github.com/rickferrdev/dotenv"
+)
+
+type Env struct {
+	AppPort      string `env:"APP_PORT"`
+	AppMode      string `env:"APP_MODE"`
+	AppSecretJWT string `env:"APP_SECRET_JWT"`
+	AppDBUrl     string `env:"APP_DB_URL"`
+}
+
+func NewEnv() (*Env, error) {
+	var env Env
+
+	dotenv.Collect()
+
+	if err := dotenv.Unmarshal(&env); err != nil {
+		return nil, err
+	}
+
+	return &env, nil
+}
