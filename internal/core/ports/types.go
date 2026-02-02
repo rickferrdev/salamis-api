5cec0d36342a2b0781f5b87643ce9840a981e43c
Henr. Ferr <rickferr.dev@gmail.com>
2026-02-01 22:38:35 -0300

    feat(ports): add Tokenizer, Hasher, and Response interfaces for authentication and HTTP handling (#5)
    * feat(ports): add interfaces and types for authentication, posts, and profiles
    
    * feat(ports): add Tokenizer, Hasher, and Response interfaces for authentication and HTTP handling

---
+++ internal/core/ports/types.go
@@ -0,0 +1,50 @@
+package ports
+
+// Data transfer objects (DTOs)
+// User related DTOs
+type UserOutput struct {
+	Email    string
+	Username string
+	Nickname string
+}
+
+// Profile related DTOs
+type ProfileInput struct {
+	Status      string
+	AvatarURL   string
+	Description string
+	UserID      uint
+}
+
+type ProfileOutput struct {
+	Status      string
+	AvatarURL   string
+	Description string
+	User        UserOutput
+}
+
+// Post related DTOs
+type PostInput struct {
+	Title    string
+	Content  string
+	AuthorID uint
+}
+
+type PostOutput struct {
+	Title   string
+	Content string
+	Author  UserOutput
+}
+
+// Auth related DTOs
+type AuthInput struct {
+	Email    string
+	Password string
+	Username string
+	Nickname string
+}
+
+type AuthOutput struct {
+	Token string
+	User  UserOutput
+}
