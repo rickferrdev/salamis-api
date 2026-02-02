87866ef8494fdd7173d9e423cf5f7854282b97d7
Henr. Ferr <rickferr.dev@gmail.com>
2026-02-01 22:34:34 -0300

    feat(ports): add Tokenizer, Hasher, and Response interfaces for authentication and HTTP handling

---
+++ internal/core/ports/packages.go
@@ -0,0 +1,28 @@
+package ports
+
+import (
+	"context"
+	"net/http"
+)
+
+// JWT Tokenizer
+type TokenPayload struct {
+	UserID uint
+}
+
+type Tokenizer interface {
+	Generate(userID uint) (string, error)
+	Verify(token string) (*TokenPayload, error)
+}
+
+// Password Hasher
+type Hasher interface {
+	Generate(password []byte) ([]byte, error)
+	Compare(hash, password []byte) error
+}
+
+// HTTP Response Handler
+type Response interface {
+	Success(ctx context.Context, w http.ResponseWriter, status int, data any)
+	Error(ctx context.Context, w http.ResponseWriter, err error)
+}
